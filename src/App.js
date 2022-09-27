1cc2c60f291654c55fce7c54065ad2dcc21493da
KevinAdamz24 <93513897+KevinAdamz24@users.noreply.github.com>
2022-09-27 16:24:29 +1000

    second commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from './Home';
+import Checkout from './Checkout';
+
+function App() {
+  return (
+    // BEM
+    <Router>
+      <div className="app">
+      <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>       
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAwyFd1khF-E_Xcl_d6MZQsPVvjV72GUs0",
+    authDomain: "clone-2468f.firebaseapp.com",
+    projectId: "clone-2468f",
+    storageBucket: "clone-2468f.appspot.com",
+    messagingSenderId: "633745986421",
+    appId: "1:633745986421:web:41b5a1a707f1b3a3a5793c",
+    measurementId: "G-CPZLNLL6SR"
+  };
